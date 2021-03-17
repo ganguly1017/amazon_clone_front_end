49ef5d1a3c1a48bbab20093f2aa85c553fd5143c
Sandeep Ganguly <sandeep@example.com>
2021-03-17 20:10:53 +0530

    seller intro translation

--- src/components/SellerAccount/SellerIntroView.js
+++ src/components/SellerAccount/SellerIntroView.js
@@ -1,39 +1,43 @@
 import React from 'react'
 
-function SellerIntroView() {
+function SellerIntroView(props) {
+
+  const { t, i18n } = props;
+  const rtl = ( i18n.languages[0] == 'pk' ? 'text-right' : '');
+
   return (
   <div className="container-fluid mb-5" >
     {/* <!-- Seller Account Intro Starts --> */}
     <div className="row">
-      <div className="col-md-12 p-5 text-white text-center" style={{backgroundColor: '#008577'}}>
-        <h1>"Since last 6 years my friends wealth increased by 3000%, thanks to selling of BSNL, LIC, AIR India & etc on Amazon."</h1>
-        <p>Narendra Modi, <em>PMO India</em> (Amazon Seller)</p>
-        <a href="your_seller_account.html" className="btn btn-warning rounded-pill">Start selling</a>
-        <p>It only takes 15 minutes to setup your account</p>
+      <div className={`col-md-12 p-5 text-white text-center ${rtl}`} style={{backgroundColor: '#008577'}}>
+        <h1>{t('seller_intro.heading')}</h1>
+        <p>{t('seller_intro.author.name')}<em>{t('seller_intro.author.role')}</em> {t('seller_intro.author.job')}</p>
+        <a href="your_seller_account.html" className="btn btn-warning rounded-pill">{t('seller_intro.btn_txt')}</a>
+        <p>{t('seller_intro.sub_title')}</p>
       </div>
     </div>
 
     <div className="row">
       <div className="col-md-12 pl-5 pr-5 pt-3 pb-2">
-        <h2>Why sell on Amazon?</h2>
+        <h2 className={`${rtl}`}>{t('seller_intro.question_txt')}</h2>
       </div>
     </div>
     
     <div className="row">
       <div className="col-md-4 p-5">
         <img src="assets/img/seller_account/img_01.svg" className="mb-2" height="100" alt="" />
-        <h3>E-Commerce: Your next big sales channel</h3>
-        <div>Start selling on Amazon - without building a website. Register now to start selling on Amazon.in.</div>
+        <h3 className={`${rtl}`}>{t('seller_intro.row.col1.title')}</h3>
+        <div className={`${rtl}`}>{t('seller_intro.row.col1.content')}</div>
       </div>
       <div className="col-md-4 p-5">
         <img src="assets/img/seller_account/img_02.svg" className="mb-2" height="100" alt="" />
-        <h3>Reach crores of customers</h3>
-        <div>Sell to crores of engaged customer visiting Amazon.in on desktop and through our mobile app.</div>
+        <h3 className={`${rtl}`}>{t('seller_intro.row.col2.title')}</h3>
+        <div className={`${rtl}`}>{t('seller_intro.row.col2.content')}</div>
       </div>
       <div className="col-md-4 p-5">
         <img src="assets/img/seller_account/img_03.svg" className="mb-2" height="100" alt="" />
-        <h3>Receive timely payments</h3>
-        <div >Amazon ensures your payments for your delivered orders are deposited directly in your bank account every 7 days.</div>
+        <h3 className={`${rtl}`}>{t('seller_intro.row.col3.title')}</h3>
+        <div className={`${rtl}`}>{t('seller_intro.row.col3.content')}</div>
       </div>
     </div>
   </div>
